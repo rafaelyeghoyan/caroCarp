2e27e4297f37d887127f4d21759d40570b3c0f89
rafaelyeghoyan <rvvebcss@gmail.com>
2024-11-10 12:17:42 +0400

    Catalouge is ready

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 let acc = document.getElementsByClassName("accordion");
+let catalogue = document.getElementsByClassName("catalogue")[0];
+let openCatalogue = document.getElementsByClassName("shopping_card")[0];
+let body = document.body;
+let isCatalogueOpen = false;
+
+openCatalogue.addEventListener("click", () => {
+    if (!isCatalogueOpen) { // Если блок закрыт
+        catalogue.style.width = "100%";
+        catalogue.style.height = "100vh";
+        body.style.overflow = "hidden";
+        isCatalogueOpen = true;
+    } else {
+        catalogue.style.width = "0";
+        catalogue.style.height = "0";
+        body.style.overflow = "";
+        isCatalogueOpen = false;
+    }
+});
+
 
 for ( let i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
